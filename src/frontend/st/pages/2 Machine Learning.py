d8a97647e0eb9cf739ccd3544293d75c8df810fa
D355I <jonabach.kontakt@gmail.com>
2025-11-28 10:17:24 +0100

    sync

--- src/frontend/st/pages/2 Machine Learning.py
+++ src/frontend/st/pages/2 Machine Learning.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from backend.data_processing.alphavantage_processed import process_alphavantage_raw_db
-
+import sklearn
+import numpy as np
 #__________________________Header____________________________
 
 st.set_page_config(page_title="Machine Learning", page_icon="📈")
